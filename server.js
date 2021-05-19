0150af7f54b1ef2f192a6387136ef6725e0ef1ce
Guilherme Trindade <guilherme.01.gt@gmail.com>
2021-05-19 19:01:25 -0300

    Insere template ejs e socket.io

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.set('views', __dirname + "/public");
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
-    res.send('CHAT');
+    res.render('index.html');
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('Socket conectado: ' + socket.id);
+});
+
+server.listen(3000, () => {
     console.log('Servidor rodando!');
 });
