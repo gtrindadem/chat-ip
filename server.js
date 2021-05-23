39758a02afbce3b8bbf067e208d0e328e11d6982
Guilherme Trindade <guilherme.01.gt@gmail.com>
2021-05-23 15:36:53 -0300

    Implenta style.css e ajustes de validacao gerais

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + "/public");
 app.engine('html', require('ejs').renderFile);
 
